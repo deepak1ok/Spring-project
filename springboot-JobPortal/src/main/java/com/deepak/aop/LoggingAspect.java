c0637059dbb039b0f42602dc513dc05a320316b2
Deepak Kumar <deepak.kr10460@gmail.com>
2025-08-23 15:47:44 +0530

    update Logging Aspect class

---
+++ springboot-JobPortal/src/main/java/com/deepak/aop/LoggingAspect.java
@@ -0,0 +1,25 @@
+package com.deepak.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAspect {
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+
+//	return_type, class_name(like., com.deepak.aop).method_name(argument)
+//  execution(*,com.deepak.services.JobService.*(..)) --> this is point cut
+//	@Before --> this is advice
+
+	@Before("execution(*,com.deepak.services.JobService.*(..))")
+	public void logMethodCall(JoinPoint jp) {
+		LOGGER.info("Method Called.. " + jp.getSignature().getName());
+	}
+
+}
